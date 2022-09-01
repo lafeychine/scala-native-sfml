3182b53d2e6a24624c31a12be4f0da5f330eb194
Vincent Lafeychine <vincent.lafeychine@proton.me>
2022-09-01 19:23:41 +0200

    Add basic functions

---
+++ src/main/scala/sfml/window/VideoMode.scala
@@ -0,0 +1,18 @@
+package sfml
+package window
+
+import scalanative.unsafe.*
+import scalanative.unsigned.{UnsignedRichInt, UnsignedRichLong}
+
+import internal.Type
+import internal.window.VideoMode.*
+
+class VideoMode(val width: Int, val height: Int, val bitsPerPixel: Int) extends StackResource[Ptr[sfVideoMode]]:
+
+    private[sfml] def bind(implicit z: Zone) =
+        val videoMode = alloc[sfVideoMode]()
+
+        videoMode._1 = width.toUInt
+        videoMode._2 = height.toUInt
+        videoMode._3 = bitsPerPixel.toUInt
+        return videoMode
