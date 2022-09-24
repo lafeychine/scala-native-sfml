01a5afc6fe62ca47b67fbb11277d6beeb946fa40
Vincent Lafeychine <vincent.lafeychine@proton.me>
2022-09-24 23:16:20 +0200

    feat(test): Add complete Scala test

--- tests/src/main/cxx/tests/Example.cpp
+++ tests/src/main/cxx/tests/Example.cpp
@@ -5,19 +5,14 @@
 
 snTest(Example)
 {
+    sf::Event event;
     sf::RenderWindow window(sf::VideoMode(1024, 768), "Test");
 
     window.isOpen();
-
-    sf::Event event;
-
-    while (window.pollEvent(event)) {
-        // std::cout << event << std::endl;
-    }
+    while (window.pollEvent(event)) {}
 
     window.clear(sf::Color(0x01, 0x23, 0x45, 0x67));
     window.display();
-    snTestScreen.takeScreenshot();
     snTestScreen.takeScreenshot();
 
     window.close();
--- tests/src/main/scala/Main.scala
+++ tests/src/main/scala/Main.scala
@@ -1,47 +1,11 @@
-import scala.util.Using
-import sfml.graphics.*
-import sfml.system.*
-import sfml.window.*
+import scala.scalanative.reflect.Reflect
+import scala.scalanative.reflect.annotation.EnableReflectiveInstantiation
 
-import java.lang.String
+@main def main(path: String, testName: String) =
+    Reflect
+        .lookupLoadableModuleClass("tests." + testName + "$")
+        .map(cls =>
+            val obj = cls.loadModule().asInstanceOf[tests.SNTest]
 
-import scalanative.unsafe.*
-import scalanative.libc.stdlib.system
-
-// import utest.*
-
-class TestScreen(path: String):
-    @SuppressWarnings(Array("org.wartremover.warts.Var"))
-    private var id_screenshot = 0;
-
-    @SuppressWarnings(Array("org.wartremover.warts.StringPlusAny"))
-    final def takeScreenshot(): Unit =
-        Zone { implicit z =>
-            system(toCString("xwd -root -silent > " + path + "/scala/" + id_screenshot))
-            id_screenshot = id_screenshot + 1
-        }
-
-@main def main(path: String) =
-    test(TestScreen(path))
-
-@SuppressWarnings(Array("org.wartremover.warts.All"))
-def test(snTestScreen: TestScreen) =
-    Using.Manager { use =>
-        val title = use(sfml.system.String("Test"))
-        val videoMode = VideoMode(1024, 768, 32)
-        val window = use(RenderWindow(videoMode, title, Window.WindowStyle.DefaultStyle))
-
-        // window.framerateLimit = 30
-
-        window.isOpen()
-
-        for event <- window.pollEvent() do println(event)
-
-        window.clear(use(Color(0x01, 0x23, 0x45, 0x67)))
-        window.display()
-
-        snTestScreen.takeScreenshot()
-        snTestScreen.takeScreenshot()
-
-        window.close()
-    }
+            obj.snTest(tests.TestScreen(path))
+        )
