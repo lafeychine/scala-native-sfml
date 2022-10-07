0e26196517ab7670d96d974b5ca6af34c61c66ea
Vincent Lafeychine <vincent.lafeychine@proton.me>
2022-10-08 00:01:28 +0200

    fix(String): Missing symbol

--- sfml/src/main/scala/sfml/internal/system/String.scala
+++ sfml/src/main/scala/sfml/internal/system/String.scala
@@ -7,12 +7,11 @@ import scalanative.unsafe.*
 import stdlib.Locale.stdLocale
 
 @link("sfml-system")
-@link("sfml-window")
 @extern private[sfml] object String:
     type sfString = CArray[Byte, Nat.Digit2[Nat._3, Nat._2]]
 
     @name("_ZN2sf6StringC2EPKcRKSt6locale")
     def ctor(self: Ptr[sfString], str: CString, locale: Ptr[stdLocale]): Unit = extern
 
-    @name("_ZN2sf6StringD2Ev")
-    def dtor(self: Ptr[sfString]): Unit = extern
+    @name("_ZdlPv")
+    def dtor(self: Ptr[Byte]): Unit = extern
