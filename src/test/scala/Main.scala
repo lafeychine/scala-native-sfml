8f19aac23757275545161c88bb6f91200fc1f03a
Vincent Lafeychine <vincent.lafeychine@proton.me>
2022-10-12 10:41:44 +0200

    fix(test): merging sfml/ and tests/ subprojects

--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -1,7 +1,12 @@
 import scala.scalanative.reflect.Reflect
 import scala.scalanative.reflect.annotation.EnableReflectiveInstantiation
 
-@main def main(path: String, testName: String) =
+@main def main(args: String*) =
+    if args.length != 2 then System.exit(0)
+
+    val path = args(0)
+    val testName = args(1)
+
     Reflect
         .lookupLoadableModuleClass("tests." + testName + "$")
         .map(cls =>
