1f2b72bd4100384eb924eb55130eaa4f208a8c42
Vincent Lafeychine <vincent.lafeychine@proton.me>
2023-01-13 00:58:17 +0100

    feat(stdlib): Add std::string allocation

---
+++ src/main/scala/sfml/internal/stdlib/String.scala
@@ -0,0 +1,11 @@
+package sfml
+package internal
+package stdlib
+
+import scalanative.unsafe.*
+
+@extern object String:
+    type stdString = CStruct3[Ptr[CString], CSize, CSize]
+
+    @name("_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEE9_M_createERmm")
+    def ctor(self: Ptr[stdString], str: CString): Unit = extern
---
+++ src/main/scala/sfml/stdlib/String.scala
@@ -0,0 +1,25 @@
+package sfml
+package stdlib
+
+import scalanative.libc.stdlib.malloc
+import scalanative.libc.string.strcpy
+import scalanative.unsafe.*
+import scalanative.unsigned.UnsignedRichInt
+
+import internal.stdlib.String.*
+
+class String private[sfml] (val string: Ptr[stdString]) extends Resource:
+
+    def this(ansiString: java.lang.String) =
+        this(malloc(sizeof[stdString] + ansiString.length().toULong).asInstanceOf[Ptr[stdString]])
+
+        string._1 = (string + 1).asInstanceOf[Ptr[CString]]
+        string._2 = ansiString.length().toULong
+        string._3 = ansiString.length().toULong
+
+        Zone { implicit z =>
+            strcpy((string + 1).asInstanceOf[CString], toCString(ansiString))
+        }
+
+    final override def close(): Unit =
+        Resource.close(string)
