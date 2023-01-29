bb3b60f43c6fafc78320e6b0bfbf683c306dbdc8
Vincent Lafeychine <vincent.lafeychine@proton.me>
2023-01-29 19:12:14 +0100

    feat(Transformable): Fully ported - except Transform

--- src/main/scala/sfml/graphics/Transformable.scala
+++ src/main/scala/sfml/graphics/Transformable.scala
@@ -4,7 +4,6 @@ package graphics
 import scalanative.unsafe.*
 
 import internal.graphics.Transformable.*
-import internal.system.Vector2.sfVector2f
 import system.Vector2f
 
 class Transformable private[sfml] (private[sfml] val transformable: Ptr[sfTransformable]) extends Resource:
@@ -12,10 +11,46 @@ class Transformable private[sfml] (private[sfml] val transformable: Ptr[sfTransf
     override def close(): Unit =
         Resource.close(dtor)(transformable)
 
+    def this() =
+        this(Resource { (r: Ptr[sfTransformable]) => ctor(r) })
+
+    /* Methods */
+
+    final def move(offsetX: Float, offsetY: Float): Unit =
+        sfTransformable_move(transformable, offsetX, offsetY)
+
+    final def move(offset: Vector2f): Unit =
+        Zone { implicit z => sfTransformable_move(transformable, offset.vector) }
+
+    final def rotate(angle: Float): Unit =
+        sfTransformable_rotate(transformable, angle)
+
+    final def scale(factorX: Float, factorY: Float): Unit =
+        sfTransformable_scale(transformable, factorX, factorY)
+
+    final def scale(factor: Vector2f): Unit =
+        Zone { implicit z => sfTransformable_scale(transformable, factor.vector) }
+
+    /* Getter / Setter */
+
+    final def origin: Vector2f =
+        val origin = sfTransformable_getOrigin(transformable)
+
+        Vector2f(origin._1, origin._2)
+
+    final def origin_=(x: Float, y: Float) =
+        sfTransformable_setOrigin(transformable, x, y)
+
+    final def origin_=(origin: Vector2f) =
+        Zone { implicit z => sfTransformable_setOrigin(transformable, origin.vector) }
+
     final def position: Vector2f =
         val pos = sfTransformable_getPosition(transformable)
 
         Vector2f(pos._1, pos._2)
+
+    final def position_=(x: Float, y: Float) =
+        sfTransformable_setPosition(transformable, x, y)
 
     final def position_=(position: Vector2f) =
         Zone { implicit z => sfTransformable_setPosition(transformable, position.vector) }
@@ -31,13 +66,8 @@ class Transformable private[sfml] (private[sfml] val transformable: Ptr[sfTransf
 
         Vector2f(scale._1, scale._2)
 
+    final def scale_=(x: Float, y: Float) =
+        sfTransformable_setScale(transformable, x, y)
+
     final def scale_=(factors: Vector2f) =
-        Zone { implicit z => sfTransformable_setScale(transformable, scale.vector) }
-
-    final def origin: Vector2f =
-        val origin = sfTransformable_getOrigin(transformable)
-
-        Vector2f(origin._1, origin._2)
-
-    final def origin_=(origin: Vector2f) =
-        Zone { implicit z => sfTransformable_setOrigin(transformable, origin.vector) }
+        Zone { implicit z => sfTransformable_setScale(transformable, factors.vector) }
--- src/main/scala/sfml/internal/graphics/Transformable.scala
+++ src/main/scala/sfml/internal/graphics/Transformable.scala
@@ -16,14 +16,39 @@ import system.Vector2
     @name("_ZN2sf13TransformableD2Ev")
     def dtor(self: Ptr[sfTransformable]): Unit = extern
 
+    /* Methods */
+
+    @name("_ZN2sf13Transformable4moveEff")
+    def sfTransformable_move(self: Ptr[sfTransformable], offsetX: CFloat, offsetY: CFloat): Unit = extern
+
+    @name("_ZN2sf13Transformable4moveERKNS_7Vector2IfEE")
+    def sfTransformable_move(self: Ptr[sfTransformable], offset: Ptr[Vector2.sfVector2f]): Unit = extern
+
+    @name("_ZN2sf13Transformable6rotateEf")
+    def sfTransformable_rotate(self: Ptr[sfTransformable], angle: CFloat): Unit = extern
+
+    @name("_ZN2sf13Transformable5scaleEff")
+    def sfTransformable_scale(self: Ptr[sfTransformable], factorX: CFloat, factorY: CFloat): Unit = extern
+
+    @name("_ZN2sf13Transformable5scaleERKNS_7Vector2IfEE")
+    def sfTransformable_scale(self: Ptr[sfTransformable], factor: Ptr[Vector2.sfVector2f]): Unit = extern
+
+    /* Getter / Setter */
+
     @name("_ZNK2sf13Transformable9getOriginEv")
     def sfTransformable_getOrigin(self: Ptr[sfTransformable]): Ptr[Vector2.sfVector2f] = extern
+
+    @name("_ZN2sf13Transformable9setOriginEff")
+    def sfTransformable_setOrigin(self: Ptr[sfTransformable], x: CFloat, y: CFloat): Unit = extern
 
     @name("_ZN2sf13Transformable9setOriginERKNS_7Vector2IfEE")
     def sfTransformable_setOrigin(self: Ptr[sfTransformable], factors: Ptr[Vector2.sfVector2f]): Unit = extern
 
     @name("_ZNK2sf13Transformable11getPositionEv")
     def sfTransformable_getPosition(self: Ptr[sfTransformable]): Ptr[Vector2.sfVector2f] = extern
+
+    @name("_ZN2sf13Transformable11setPositionEff")
+    def sfTransformable_setPosition(self: Ptr[sfTransformable], x: CFloat, y: CFloat): Unit = extern
 
     @name("_ZN2sf13Transformable11setPositionERKNS_7Vector2IfEE")
     def sfTransformable_setPosition(self: Ptr[sfTransformable], pos: Ptr[Vector2.sfVector2f]): Unit = extern
@@ -36,6 +61,9 @@ import system.Vector2
 
     @name("_ZNK2sf13Transformable8getScaleEv")
     def sfTransformable_getScale(self: Ptr[sfTransformable]): Ptr[Vector2.sfVector2f] = extern
+
+    @name("_ZN2sf13Transformable8setScaleEff")
+    def sfTransformable_setScale(self: Ptr[sfTransformable], x: CFloat, y: CFloat): Unit = extern
 
     @name("_ZN2sf13Transformable8setScaleERKNS_7Vector2IfEE")
     def sfTransformable_setScale(self: Ptr[sfTransformable], factors: Ptr[Vector2.sfVector2f]): Unit = extern
