f251e8aa54464db9e7907b2fe22f7bd2b09fce29
Vincent Lafeychine <vincent.lafeychine@proton.me>
2023-02-03 13:24:11 +0100

    chore(Event): methods private only

--- src/main/scala/sfml/window/Event.scala
+++ src/main/scala/sfml/window/Event.scala
@@ -56,70 +56,70 @@ object Event:
             case EventType.SensorChanged          => Option(SensorChanged(event.asInstanceOf[Ptr[sfSensorEvent]]))
             case _                                => None
 
-    private[sfml] object Resized:
-        def apply(event: Ptr[sfSizeEvent]): Resized =
+    object Resized:
+        private[sfml] def apply(event: Ptr[sfSizeEvent]): Resized =
             Resized(event._2.toInt, event._3.toInt)
 
-    private[sfml] object TextEntered:
-        def apply(event: Ptr[sfTextEvent]): TextEntered =
+    object TextEntered:
+        private[sfml] def apply(event: Ptr[sfTextEvent]): TextEntered =
             TextEntered(event._2.toInt)
 
-    private[sfml] object KeyPressed:
-        def apply(event: Ptr[sfKeyEvent]): KeyPressed =
+    object KeyPressed:
+        private[sfml] def apply(event: Ptr[sfKeyEvent]): KeyPressed =
             KeyPressed(Keyboard.Key.fromOrdinal(event._2 + 1), event._3, event._4, event._5, event._6)
 
-    private[sfml] object KeyReleased:
-        def apply(event: Ptr[sfKeyEvent]): KeyReleased =
+    object KeyReleased:
+        private[sfml] def apply(event: Ptr[sfKeyEvent]): KeyReleased =
             KeyReleased(Keyboard.Key.fromOrdinal(event._2 + 1), event._3, event._4, event._5, event._6)
 
-    private[sfml] object MouseWheelScrolled:
-        def apply(event: Ptr[sfMouseWheelScrollEvent]): MouseWheelScrolled =
+    object MouseWheelScrolled:
+        private[sfml] def apply(event: Ptr[sfMouseWheelScrollEvent]): MouseWheelScrolled =
             MouseWheelScrolled(Mouse.Wheel.fromOrdinal(event._2), event._3, event._4, event._5)
 
-    private[sfml] object MouseButtonPressed:
-        def apply(event: Ptr[sfMouseButtonEvent]): MouseButtonPressed =
+    object MouseButtonPressed:
+        private[sfml] def apply(event: Ptr[sfMouseButtonEvent]): MouseButtonPressed =
             MouseButtonPressed(Mouse.Button.fromOrdinal(event._2), event._3, event._4)
 
-    private[sfml] object MouseButtonReleased:
-        def apply(event: Ptr[sfMouseButtonEvent]): MouseButtonReleased =
+    object MouseButtonReleased:
+        private[sfml] def apply(event: Ptr[sfMouseButtonEvent]): MouseButtonReleased =
             MouseButtonReleased(Mouse.Button.fromOrdinal(event._2), event._3, event._4)
 
-    private[sfml] object MouseMoved:
-        def apply(event: Ptr[sfMouseMoveEvent]): MouseMoved =
+    object MouseMoved:
+        private[sfml] def apply(event: Ptr[sfMouseMoveEvent]): MouseMoved =
             MouseMoved(event._2, event._3)
 
-    private[sfml] object JoystickButtonPressed:
-        def apply(event: Ptr[sfJoystickButtonEvent]): JoystickButtonPressed =
+    object JoystickButtonPressed:
+        private[sfml] def apply(event: Ptr[sfJoystickButtonEvent]): JoystickButtonPressed =
             JoystickButtonPressed(event._2.toInt, event._3.toInt)
 
-    private[sfml] object JoystickButtonReleased:
-        def apply(event: Ptr[sfJoystickButtonEvent]): JoystickButtonReleased =
+    object JoystickButtonReleased:
+        private[sfml] def apply(event: Ptr[sfJoystickButtonEvent]): JoystickButtonReleased =
             JoystickButtonReleased(event._2.toInt, event._3.toInt)
 
-    private[sfml] object JoystickMoved:
-        def apply(event: Ptr[sfJoystickMoveEvent]): JoystickMoved =
+    object JoystickMoved:
+        private[sfml] def apply(event: Ptr[sfJoystickMoveEvent]): JoystickMoved =
             JoystickMoved(event._2.toInt, Joystick.Axis.fromOrdinal(event._3), event._4)
 
-    private[sfml] object JoystickConnected:
-        def apply(event: Ptr[sfJoystickConnectEvent]): JoystickConnected =
+    object JoystickConnected:
+        private[sfml] def apply(event: Ptr[sfJoystickConnectEvent]): JoystickConnected =
             JoystickConnected(event._2.toInt)
 
-    private[sfml] object JoystickDisconnected:
-        def apply(event: Ptr[sfJoystickConnectEvent]): JoystickDisconnected =
+    object JoystickDisconnected:
+        private[sfml] def apply(event: Ptr[sfJoystickConnectEvent]): JoystickDisconnected =
             JoystickDisconnected(event._2.toInt)
 
-    private[sfml] object TouchBegan:
-        def apply(event: Ptr[sfTouchEvent]): TouchBegan =
+    object TouchBegan:
+        private[sfml] def apply(event: Ptr[sfTouchEvent]): TouchBegan =
             TouchBegan(event._2.toInt, event._3, event._4)
 
-    private[sfml] object TouchMoved:
-        def apply(event: Ptr[sfTouchEvent]): TouchMoved =
+    object TouchMoved:
+        private[sfml] def apply(event: Ptr[sfTouchEvent]): TouchMoved =
             TouchMoved(event._2.toInt, event._3, event._4)
 
-    private[sfml] object TouchEnded:
-        def apply(event: Ptr[sfTouchEvent]): TouchEnded =
+    object TouchEnded:
+        private[sfml] def apply(event: Ptr[sfTouchEvent]): TouchEnded =
             TouchEnded(event._2.toInt, event._3, event._4)
 
-    private[sfml] object SensorChanged:
-        def apply(event: Ptr[sfSensorEvent]): SensorChanged =
+    object SensorChanged:
+        private[sfml] def apply(event: Ptr[sfSensorEvent]): SensorChanged =
             SensorChanged(Sensor.Type.fromOrdinal(event._2), event._3, event._4, event._5)
